d3d09b9efebc7bd948041b13962fc2c98522ab97
dychen098 <54570204+dychen098@users.noreply.github.com>
2019-10-28 15:25:17 -0400

    class

--- class/days/10-28-19/script.js
+++ class/days/10-28-19/script.js
@@ -1,15 +1,34 @@
-async function special(){
-    console.log("in special begining")
-    let result = await setTimeout(doStuff, 2000);
-    console.log("in special ending")
-}
+async function displayShoe(){
 
-function doStuff(){
-    console.log("in do stuff")
+    let response = await fetch("https://dychen098.github.io/class/days/10-28-19/shoe.json");
+    let shoeJson = await response.json();
+
+    let shoeName = shoeJson.name;
+    let shoeBrand = shoeJson.brand;
+    let comments = shoeJson.comments;
+
+    console.log(`${shoeName} by ${shoeBrand}`)
+
+    for(i in comments){
+        // let write = document.createElement("p")
+        // write.innerHTML = comments[i];
+        console.log(comments[i]);
+    }
+
+    //append a section with the info about the shoe to your page
+    let section = document.createElement("section")
+    section.innerHTML = `${shoeName} by ${shoeBrand}`;
+    let content = document.getElementById("content");
+    content.append(section);
 }
 
 window.onload = function(){
-    this.console.log("before calling special");
-    this.special();
-    this.console.log("after callilng special");
+    this.displayShoe();
+
+    // append a h1 to page 
+    let content = document.getElementById("content");
+    let h1 = document.createElement("h1");
+    h1.innerHTML =  "I love shoes"
+    content.append(h1);
+
 }
